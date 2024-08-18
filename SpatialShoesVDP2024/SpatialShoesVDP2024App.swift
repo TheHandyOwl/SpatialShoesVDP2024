e194ce77093c69f36de749ea12e9fd0b6b32ba82
Carlos Company <carlos.company@gigigo.com>
2024-08-18 18:53:23 +0200

    Initial Commit

---
+++ SpatialShoesVDP2024/SpatialShoesVDP2024App.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialShoesVDP2024App.swift
+//  SpatialShoesVDP2024
+//
+//  Created by Carlos Company on 18/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialShoesVDP2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
